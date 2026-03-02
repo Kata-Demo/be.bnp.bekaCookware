74ef59c57790ae97df59726f6b99e03a845c3c7a
Kata-Demo <jnunkovics@testmind.be>
2026-03-02 22:56:49 +0100

    test in main java

--- kata-demo/src/main/java/be/bnp/beka/cookware/Main.java
+++ kata-demo/src/main/java/be/bnp/beka/cookware/Main.java
@@ -1,11 +1,12 @@
-
+package be.bnp.beka.cookware;
 import org.openqa.selenium.WebDriver;
 
-import utils.ConfigLoader;
-import utils.WebDriverFactory;
-import pages.CookieConsentPopUpPage;
-import pages.FlyoutModalPage;
-import pages.GeolocationModalPage;
+import be.bnp.beka.cookware.utils.ConfigLoader;
+import be.bnp.beka.cookware.utils.WebDriverFactory;
+import be.bnp.beka.cookware.utils.typedEnum.Browser;
+import be.bnp.beka.cookware.utils.typedEnum.Device;
+import be.bnp.beka.cookware.utils.typedEnum.Lang;
+import be.bnp.beka.cookware.pages.HomePage;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,25 +16,15 @@ public class Main {
         String env = cfg.getEnvironment();                          // e.g., "test"
         String baseUrl = cfg.get("base.url");                  // resolved with precedence
 
-        System.out.println("Hello Tester!");
-        System.out.println("You are configured to use: " + env );
-        System.out.println("Your base url is : " + baseUrl );
-        WebDriver wd = WebDriverFactory.create("PHONE", "CHROME", "EN");
+        WebDriver wd = WebDriverFactory.create(Device.PHONE, Browser.CHROME, Lang.EN);
         //WebDriver wd = WebDriverFactory.create("TABLET", "FIREFOX", "NL");
         //WebDriver wd = WebDriverFactory.create("DESKTOP", "FIREFOX", "DE");
         //nav to base url
         wd.get(baseUrl);
-        CookieConsentPopUpPage CC = new CookieConsentPopUpPage(wd);
-        //accept cookie
-        CC.accept();
-        GeolocationModalPage GM = new GeolocationModalPage(wd);
-        //allow geoloc
-        GM.allow();
-        FlyoutModalPage FM = new FlyoutModalPage(wd);
-        //wait for marketing add to be visible
-        System.out.println("is marketing modal displayed : " + FM.waitForVisible() );
-        //close marketing add
-        FM.close();
-        System.out.println("is marketing modal displayed after close : " + FM.isDisplayed() );
+        HomePage Hp = new HomePage(wd);
+        Hp.acceptCookie();
+        Hp.allowGeoloc();
+        Hp.closeMarketingAdd();
+
     }
 }
